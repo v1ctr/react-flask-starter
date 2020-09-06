f02f03bd4da2998a9543941288f440b6477d143e
victor <v1ctr@icloud.com>
2020-09-06 21:17:05 +0200

    added database and authentication routes

--- server/README.md
+++ server/README.md
@@ -3,3 +3,52 @@
     $ python3 -m venv venv
     $ source venv/bin/activate
     (venv) $ _
+
+## Database Migration with Flask-Migrate
+
+### Create a Migration Script
+    $ flask db migrate -m "migration message"
+    
+### Upgrading the Database
+    $ flask db upgrade
+
+## Flask Python Shell
+
+### Start Flask Shell
+    $ flask shell
+
+### Exit Flask Shell
+    $ exit()
+    
+### Creating/Removing tables
+Use with caution. All data in database will be lost.
+
+    $ db.create_all()
+    $ db.drop_all()
+    
+### Creating Users
+    $ user = User("test@gmail.com")
+    $ db.session.add(user)
+    $ db.session.commit()
+
+### Querying Users
+Get all users
+
+    $ User.query.all()
+
+Get specific user
+
+    $ User.query.filter_by(email="test@gmail.com").first()
+
+Get native SQL query that SQLAlchemy generates
+    
+    $ str(User.query.filter_by(email="test@gmail.com"))
+
+## Testing
+### Unit Tests
+    $ flask test
+
+### Code Coverage
+Code coverage tools measure how much of the application is excercised by unit tests. (Grinberg. M, 2018, Flask Web Development, O'REILLY)
+
+    $ flask test --coverage
---
+++ server/config.py
@@ -0,0 +1,44 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+
+    SSL_REDIRECT = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance/dev-instance.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance/test-instance.sqlite')
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance/instance.sqlite')
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}

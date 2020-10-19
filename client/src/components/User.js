ac6e89637ad755f1e08921701f22f066ed9f0837
victor <v1ctr@icloud.com>
2020-10-19 08:33:28 +0200

    added view for user information

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,10 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Descriptions, Spin, Alert } from 'antd';
+import API from '../utils/API';
 
 function User() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [user, setUser] = useState({});
 
-  return (
-    <h1>User</h1>
-  );
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let result = await API.get('/user/me');
+        if (result.status === 200) {
+          setUser(result.data);
+        } else {
+          console.log("failed");
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    setLoading(true);
+    fetchData();
+  }, [])
+
+  if (loading) {
+    return (<Spin />);
+  } else {
+    return (
+      <React.Fragment>
+        {error &&
+          <Alert
+            message="Error"
+            description={error.message}
+            type="error"
+            closable
+          />
+        }
+        <Descriptions title="User Info">
+          <Descriptions.Item label="ID">{user.id}</Descriptions.Item>
+          <Descriptions.Item label="E-Mail">{user.email}</Descriptions.Item>
+          <Descriptions.Item label="First Name">{user.first_name}</Descriptions.Item>
+          <Descriptions.Item label="Last Name">{user.last_name}</Descriptions.Item>
+          <Descriptions.Item label="Created at">{user.created_at}</Descriptions.Item>
+        </Descriptions>
+      </React.Fragment>
+    );
+  }
 }
 
 export default User;
--- server/app/user/routes.py
+++ server/app/user/routes.py
@@ -1,12 +1,20 @@
 from . import user_blueprint
+from app.models import User
+import uuid
 from flask import jsonify
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 @user_blueprint.route('/me', methods=['GET'])
 @jwt_required
 def get_me():
+    current_user_id = get_jwt_identity()
+    user = User.query.filter_by(id=uuid.UUID(current_user_id)).first_or_404()
     response_data = {
-        'success': True
+        'id': user.id,
+        'email': user.email,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'created_at': user.created_at
     }
     return jsonify(response_data), 200
